6055fb01f48ff43458df24b7050716f981bc4c40
Th3On3 <fmfrodrigues@gmail.com>
2020-12-23 21:58:09 +0000

    Small batch file fix.

--- SnapWindow.bat
+++ SnapWindow.bat
@@ -1,2 +1,13 @@
 @echo off
-start bin/AutoHotKey.exe SnapWindow.ahk
+if exist bin/AutoHotKey.exe (
+    start bin/AutoHotKey.exe SnapWindow.ahk > nul 2> nul
+) else (
+    for %%X in (AutoHotKey.exe) do (set FOUND=%%~$PATH:X)
+    if defined FOUND (
+        start AutoHotKey.exe SnapWindow.ahk > nul 2> nul
+    )
+    if not defined FOUND (
+        echo Please put AutoHotKey.exe on bin/ directory or install AutoHotKey
+        pause 
+    )
+) 
